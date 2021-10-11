9c739835509e2456b4c5acbc738634acca8f958c
root <root@asteramk.ru-central1.internal>
2021-10-11 16:45:30 +0500

    origin

---
+++ bot_back_end/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BotBackEndConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bot_back_end'
---
+++ bot_back_end/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Three_categories(models.Model):
+    text = models.CharField('key word', max_length=50)
+    category = models.CharField('category of word (or depart)', max_length=50)
+
+
+class Category_managers(models.Model):
+    text = models.CharField('key word', max_length=50)
+    name_man = models.CharField('name of manager', max_length=100)
+    phone_number = models.CharField(max_length=12)
+
+
+class Code_filial_2_name_filial(models.Model):
+    code_filial = models.IntegerField('city code of filial')
+    name = models.CharField('name of city filial', max_length=100)
+
+
+class Depart_filial_2_phone_number(models.Model):
+    depart = models.CharField('name of depart (or category)', max_length=50)
+    filial = models.CharField('name of city filial', max_length=50)
+    phone_number = models.IntegerField('phone number')
+    info = models.CharField('info about depart', max_length=50)
