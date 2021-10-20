d76574fbcedd3cfbe7986c62296d825b3ec8ed09
root <root@asteramk.ru-central1.internal>
2021-10-20 16:42:39 +0500

    удалена new БД

--- bot_django/settings.py
+++ bot_django/settings.py
@@ -78,14 +78,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'new': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'amkaster',
-        'USER': 'callbot',
-        'PASSWORD': 'notallme',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
     }
 }
 
