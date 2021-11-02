fede2e5483928715525375738f606203f8700366
root <root@asteramk.ru-central1.internal>
2021-11-02 15:16:36 +0500

    api: отображение региона в calltrack

--- bot_back_end/api/serializers.py
+++ bot_back_end/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from ..models import Calltrack_lite, Departs
+from ..models import Calltrack_lite, Departs, Regions_name_and_code
 
 
 class Calltrack_liteSerializer(serializers.ModelSerializer):
@@ -13,11 +13,15 @@ class Calltrack_liteSerializer(serializers.ModelSerializer):
 class Calltrack_liteDetailSerializer(serializers.ModelSerializer):
 
     depart = serializers.SerializerMethodField()
+    region_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Calltrack_lite
         fields = '__all__'
 
+    @staticmethod
+    def get_region_name(obj):
+        return Regions_name_and_codeSerializer(Regions_name_and_code.objects.filter(code_region=obj.region), many=True).data
     @staticmethod
     def get_depart(obj):
         return DepartsSerializer(Departs.objects.filter(depart_added_phone_number=obj.dial_route), many=True).data
@@ -29,3 +33,9 @@ class DepartsSerializer(serializers.ModelSerializer):
         model = Departs
         fields = '__all__'
 
+
+class Regions_name_and_codeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Regions_name_and_code
+        fields = '__all__'
--- bot_back_end/api/views.py
+++ bot_back_end/api/views.py
@@ -3,19 +3,23 @@ from rest_framework import viewsets
 from .serializers import (
     Calltrack_liteSerializer,
     DepartsSerializer,
-    Calltrack_liteDetailSerializer)
+    Calltrack_liteDetailSerializer,
+    Regions_name_and_codeSerializer)
 
-from ..models import Calltrack_lite, Departs
+from ..models import (
+    Calltrack_lite,
+    Departs,
+    Regions_name_and_code)
 
 class Calltrack_liteViewSet(viewsets.ModelViewSet):
 
     queryset = Calltrack_lite.objects.all()
     serializer_class = Calltrack_liteSerializer
-    
+
     action_to_serializer = {
         'retrieve': Calltrack_liteDetailSerializer
     }
-    
+
     def get_serializer_class(self):
         return self.action_to_serializer.get(
             self.action,
@@ -27,3 +31,8 @@ class DepartViewSet(viewsets.ModelViewSet):
 
     queryset = Departs.objects.all()
     serializer_class = DepartsSerializer
+
+class Regions_name_and_codeViewSet(viewsets.ModelViewSet):
+
+    queryset = Regions_name_and_code.objects.all()
+    serializer_class = Regions_name_and_codeSerializer
